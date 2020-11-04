a0f40d828be3c3a3cf5a19dc1de9d31eb072c8ab
ksh0517 <50390857+ksh0517@users.noreply.github.com>
2020-11-04 15:14:45 +0900

    layout 구성

--- src/main/java/com/godcoder/myhome/Controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/Controller/BoardController.java
@@ -1,15 +1,27 @@
 package com.godcoder.myhome.Controller;
 
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+//        findAll 데이터를 다 가져올수있음
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 }
